1239e995dda4d0a566ac8e14f774a24be923b32a
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-12-20 15:18:13 +0100

    add blacklist

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ default_allowed_extensions_path = os.path.join((os.path.dirname(os.path.realpath
 with open(default_allowed_extensions_path,"r") as f:
     default_allowed_extensions = [extension.lower() for extension in f.read().split("\n")]
 
-def count_dir(dir,allowed_extensions,result={}):
+def count_dir(dir,allowed_extensions,blacklist,result={}):
     items = os.listdir(dir)
     for item in items:
         if os.path.isdir(item):
-            count_dir(os.path.join(dir,item), allowed_extensions, result)
+            count_dir(os.path.join(dir,item), allowed_extensions, blacklist, result)
         else:
             if not "." in item:
                 continue
-            extension = item.split(".")[-1]
+            if item in blacklist:
+                print("wea")
+                continue
+            extension = item.split(".")[-1].lower()
             if extension in allowed_extensions:
                 with open(os.path.join(dir,item), "r") as f:
                     count = len(f.readlines())
@@ -22,18 +25,19 @@ def count_dir(dir,allowed_extensions,result={}):
     return result
 
 @arguably.command
-def scan(directory=os.getcwd(),allowed_extensions:list[str]=None):
+def scan(directory=os.getcwd(),blacklist:list[str]=[],allowed_extensions:list[str]=None):
     """
     count the number of lines of code in a directory its subdirectories
 
     Args:
         directory: directory to run scan on, defaults to current dir.
+        blacklist: list of files to ignore.
         allowed_extensions: overrides which extensions to allow. default (None) will use standard set.
     """
     if not allowed_extensions:
         allowed_extensions = default_allowed_extensions
 
-    print(count_dir(directory,allowed_extensions))
+    print(count_dir(directory,allowed_extensions,blacklist))
     
 
 if __name__ == "__main__":
